91ff5b42b29804ea071b92b878694d63d2adab1a
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-01-26 23:58:39 +0200

    @fix(automation): fix mechanism for deploying pkg to github release
    `curl` program didn't take the gh asset address as a string with quotes & the name of the .tar.gz archive wasn't properly referenced
    
    Fixes #16

--- assets_release.sh
+++ assets_release.sh
@@ -5,14 +5,14 @@
 pushd src
 
 if [[ -f release_id.txt ]]; then
-  ID=(cat release_id.txt)
+  ID=$(cat release_id.txt)
   PYTHON_PKG_NAME="PythonPackage"
   PYTHON_PKG_FILENAME=$(ls dist | head -n 1)
 
   echo "Publishing python wheel with GitHub API"
-  curl https://uploads.github.com/repos/$TRAVIS_REPO_SLUG/releases/$ID/assets?name=$PYTHON_PKG_NAME&label=python-wheel&access_token=$GH_TOKEN \
-  --header "Content-Type: application/json" \
-  --data-binary @"dist/PYTHON_PKG_FILENAME"
+  curl "https://uploads.github.com/repos/$TRAVIS_REPO_SLUG/releases/$ID/assets?name=$PYTHON_PKG_NAME&label=python-targz&access_token=$GH_TOKEN" \
+  --header "Content-Type: application/octet-stream" \
+  --data-binary @"dist/$PYTHON_PKG_FILENAME"
 fi
 
 popd
