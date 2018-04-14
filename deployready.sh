4743c6a98f8a70d54d9357f51cf3c34d50d4a19e
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-04-14 21:24:06 +0300

    fix - trying to make packagecloud not upload pkgs when it's not needed

--- deployready.sh
+++ deployready.sh
@@ -2,6 +2,9 @@
 # then abort the build, which won't affect the build status anyway
 pushd src
 if [[ ! -f release_id.txt ]]; then
+  echo "No packages are being pushed to packagecloud.io"
   exit 4
+else
+  echo "Found packages to be pushed to packagecloud.io"
 fi
 popd
